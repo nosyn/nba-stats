58bd2dedf3ed4cb5575c27f7ef6153b7de7bed2e
sonplusone <son.nguyen@plusonerobotics.com>
2022-04-24 14:58:20 -0500

    Add esbuild for node server

--- client/src/graphql/client/index.ts
+++ client/src/graphql/client/index.ts
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { __prod__ } from "../../constants";
 
 const client = new ApolloClient({
-  uri: "/api/graphql",
+  uri: __prod__ ? "/graphql" : "/api/graphql",
   cache: new InMemoryCache(),
 });
 
