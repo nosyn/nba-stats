7a439ce449fabee5c0bc48872bfe4e8d32053b96
sonplusone <son.nguyen@plusonerobotics.com>
2022-04-23 22:51:48 -0500

    Add docker-compose dev env

--- client/src/graphql/client/index.ts
+++ client/src/graphql/client/index.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:5000/graphql",
+  uri: "/api/graphql",
   cache: new InMemoryCache(),
 });
 
